943f1884997b2fef3658391000f08c2a7e2d2833
bikevin <kevin.bi.1002@gmail.com>
2016-08-25 18:21:49 -0400

    added graph stuff

--- demo.js
+++ demo.js
@@ -3,6 +3,16 @@
  */
 
 var _data = [];
+var _categorical = [];
+var _table;
+var _classes = [];
+var _layerArray = [];
+var _isinput = [];
+var _network;
+var _error = [];
+var _errorMinMax = [Number.MAX_VALUE, Number.MIN_VALUE];
+var _x, _y;
+var _skips = 1;
 
 
 $(document).ready(function(){
@@ -28,12 +38,61 @@ $(document).ready(function(){
     $("#resetUnused").click(function(){
         $("#choose").find('div.list-group-item:not(.listHeader)').appendTo($("#unused"));
     });
-    
-    $("#buildStart").click(function(){
+
+    $("#check").click(function(){
         buildCategoryChecker();
     });
-    
-    
+
+    $("#buildStart").click(function(){
+        getCategoricalFromChecker();
+        var variables  = getFromCategorySetter();
+        var out = toFinalData(_data, _categorical, variables.input, variables.output);
+        _data = out.data;
+        _classes = out.classes;
+        _isinput = out.isInput;
+        _table.loadData(_data);
+        populateLayerWizard();
+        $(this).hide();
+        $("#networkWizard").show();
+        $("#startTraining").show();
+        $("#trainingOptions").show();
+    });
+
+    $("#addLayer").click(function(){
+
+        var layers = $('#notOutputLayer');
+
+        var newDiv = $('<div class="list-group-item">').appendTo(layers);
+
+        var numLayers = layers.find('div').length - 1;
+
+        newDiv.html('<span>Hidden Layer ' + numLayers + '</span>').attr('id', numLayers).data('type', 'hidden')
+            .data('size', 1).data('nonlin', 'ReLU').click(function(){
+            $('#layerOptions').data('current', numLayers);
+            $("#layerType").text($(this).data('type'));
+            $("#size").val($(this).data('size'));
+            $("#nonlin").val($(this).data('nonlin'));
+        });
+
+        newDiv.click();
+    });
+
+    $("#layerType, #size, #nonLinearity").change(function(){
+        var currentLayerNum = $("#layerOptions").data('current');
+        var currentLayer = $("#" + currentLayerNum + ".list-group-item");
+
+        currentLayer.data('size', $("#size").val()).data('nonlin', $("#nonlin").val());
+    });
+
+    $("#trainStart").click(function(){
+        generateLayerArray();
+        _network = createNetwork(_layerArray);
+        trainNetwork(_network, createTrainingData(_data, _isinput), getTrainingInfo());
+        createErrorGraph();
+        $("#trainStats").show();
+    })
+
+
 
 });
 
@@ -49,7 +108,7 @@ function startDemo(){
 
             results['data'] = results['data'].filter(function(n){return n.length != 0;});
 
-            bindToTable($("#dataTable")[0], results['data']);
+            _table = bindToTable($("#dataTable")[0], results['data']);
 
             _data = results['data'];
         },
@@ -106,7 +165,7 @@ function populateUnused(){
 }
 
 function buildCategoryChecker(){
-    var categorical = isCategorical(_data);
+    _categorical = isCategorical(_data);
 
     var indepBase = $("#indepChecker");
     var depBase = $("#depChecker");
@@ -121,7 +180,7 @@ function buildCategoryChecker(){
         alert('You need dependent variables!');
         return -1;
     } else {
-        $("#networkBuild").show();
+        $("#category, #networkBuild").show();
         $("#buildDialog").hide();
     }
 
@@ -143,8 +202,6 @@ function buildCategoryChecker(){
 
         $("<span>").text('Categorical').appendTo(label);
 
-        $("<div style='width: 5px'>").appendTo(div);
-
         label = $("<label class='radio-inline'>")
             .appendTo(radioDiv);
 
@@ -154,7 +211,7 @@ function buildCategoryChecker(){
         $("<span>").text('Continuous').appendTo(label);
 
 
-        if(categorical[$(predictors[i]).text()]){
+        if(_categorical[$(predictors[i]).text()]){
             $(div.find('input').get(0)).prop('checked', true);
         } else {
             $(div.find('input').get(1)).prop('checked', true);
@@ -185,15 +242,257 @@ function buildCategoryChecker(){
 
         $("<span>").text('Continuous').appendTo(label);
 
-        if(!categorical[$(predicted[i]).text()]){
+        if(_categorical[$(predicted[i]).text()]){
             $(div.find('input').get(0)).prop('checked', true);
         } else {
             $(div.find('input').get(1)).prop('checked', true);
         }
     }
 
+}
+
+function getCategoricalFromChecker(){
+    if(_categorical.length == 0){
+        return -1;
+    }
+
+    $.each(_categorical, function(index, value){
+        var inputs = $("#category")
+            .find('input[id=' + (index + 1) + ']:checked').parent();
+        if(inputs.length == 1){
+            _categorical[index] = inputs.find('span').text() == 'Categorical';
+        }
+    });
+}
+
+function populateLayerWizard(){
+
+    var inputLength = 0;
+    var outputLength = 0;
+
+    $.each(_isinput, function(index, value){
+        if(value){
+            inputLength++;
+        } else {
+            outputLength++;
+        }
+    });
+
+    $("#0").data('size', inputLength);
+    $("#end").data('size', outputLength);
+
+    $("#layerList").find('.list-group-item').click(function(){
+        $('#layerOptions').data('current', $(this).attr('id'));
+        $("#layerType").text($(this).data('type'));
+        $("#size").val($(this).data('size'));
+        $("#nonlin").val($(this).data('nonlin'));
+    });
+
+}
+
+function generateLayerArray(){
+
+    _layerArray = [];
+
+    var layers = $("#layerList").find('div.list-group-item:not([id="addLayer"])');
+
+    layers.each(function(index, value){
+
+        var valueJQ = $(value);
+
+        var size = valueJQ.data('size');
+        var nonlin = valueJQ.data('nonlin');
+
+        var layer = new synaptic.Layer(size);
+
+        if(valueJQ.attr('id') != '0'){
+            layer.set({squash: returnNonlin(nonlin)})
+        }
+
+        _layerArray.push(layer);
+
+    });
+}
+
+function returnNonlin(nonlin){
+
+    if(nonlin == 'identity'){
+        return synaptic.Neuron.squash.IDENTITY;
+    } else if (nonlin == 'TanH'){
+        return synaptic.Neuron.squash.TANH;
+    } else if (nonlin == 'Sigmoid'){
+        return synaptic.Neuron.squash.LOGISTIC;
+    } else if (nonlin == 'ReLU'){
+        return synaptic.Neuron.squash.RELU;
+    }
+
+}
+
+function getFromCategorySetter(){
+
+    var inputs = $("#indepChecker").find('div:not(.listHeader)').children('span');
+    var outputs = $("#depChecker").find('div:not(.listHeader)').children('span');
+
+    var inputArray = [];
+
+    inputs.each(function(index, value){
+        var pos = parseInt($(value).text()) - 1;
+        inputArray.push(pos);
+    });
+
+    var outputArray = [];
+
+    outputs.each(function(index, value){
+       outputArray.push(parseInt($(value).text()) - 1);
+    });
+
+    return {input:inputArray, output:outputArray};
+
+}
+
+function getTrainingInfo(){
+    return {rate: parseFloat($("#rate").val()),
+            iterations: parseFloat($("#iterations").val()),
+            error: parseFloat($("#error").val())};
+}
 
 
+function trainNetwork(network, trainingData, trainingInfo){
 
+    var trainer = new synaptic.Trainer(network);
+
+    _error = [];
+
+    _errorMinMax = [Number.MAX_VALUE, Number.MIN_VALUE];
+
+    trainer.trainAsync(trainingData, {
+        rate: Math.pow(10, -1 * trainingInfo.rate),
+        iterations: trainingInfo.iterations,
+        error: Math.pow(10, -1 * trainingInfo.error),
+        cost: synaptic.Trainer.cost.MSE,
+        schedule:{
+            every: 1,
+            do: function(data){
+                if(data.iterations % _skips == 0){
+                    _error.push([data.iterations, data.error]);
+                }
+
+                if(data.error > _errorMinMax[1]){
+                    _errorMinMax[1] = data.error;
+                }
+
+                if(data.error < _errorMinMax[0]){
+                    _errorMinMax[0] = data.error;
+                    $("#minIter").find("span").text(data.iterations);
+                }
+
+                if(_error.length % 5 == 0){
+
+                    if(_error.length > 1000){
+
+                        _error = _error.filter(function(value, index){
+                            return (index % 2 == 0 || (value[1] == _errorMinMax[0] || value[1] == _errorMinMax[1]));
+                        });
+
+                        _skips++;
+
+                    }
+
+                    updateErrorGraph();
+                }
+
+
+                $("#currError").find("span").text(round(data.error, 7));
+                $("#minError").find("span").text(round(_errorMinMax[0], 7));
+                $("#currIter").find("span").text(data.iterations);
+
+
+            }
+        }
+    }).then(results => console.log('done!', results));
+
+}
+
+function createErrorGraph(){
+
+    $("#errorGraph").html('');
+
+    _x = d3.scaleLinear().range([0, 500]);
+    _y = d3.scaleLinear().range([250, 0]);
+
+    _x.domain([0, _error.length]);
+    _y.domain(_errorMinMax);
+
+    var xAxis = d3.axisBottom(_x);
+    var yAxis = d3.axisRight(_y);
+
+    var line = d3.line()
+        .x(function(d){
+            return _x(d[0]);
+        })
+        .y(function(d){
+            return _y(d[1]);
+        });
+
+    var svg = d3.select('#errorGraph').append('svg')
+        .attr('width', 600).attr('height', 320)
+        .append('g')
+        .attr('transform', 'translate(20, 20)');
+
+    svg.append('clipPath')
+        .attr('id', 'clip')
+        .append('rect')
+        .attr('width', 500)
+        .attr('height', 250);
+
+    svg.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0, 250)')
+        .call(xAxis);
+
+    svg.append('g')
+        .attr('class', 'y axis')
+        .attr('transform', 'translate(500, 0)')
+        .call(yAxis);
+
+    svg.append('path')
+        .attr('class', 'line')
+        .attr('d', line(_error));
+
+    svg.append("text")
+        .attr("text-anchor", "end")
+        .attr("x", 275)
+        .attr("y", 280)
+        .text("Iteration");
+
+    svg.append("text")
+        .attr("text-anchor", "end")
+        .attr("y", 550)
+        .attr("x", -110)
+        .attr("dy", ".75em")
+        .attr("transform", "rotate(-90)")
+        .text("Error");
+}
+
+function updateErrorGraph(){
+
+    var line = d3.line()
+        .x(function(d){
+
+            _x.domain([0, _error[_error.length - 1][0]]);
+            return _x(d[0]);
+        })
+        .y(function(d){
+
+            _y.domain(_errorMinMax);
+
+            return _y(d[1]);
+        });
+
+    var svg = d3.select('#errorGraph svg');
+
+    svg.select('.line').attr('d', line(_error));
+    svg.select('.x.axis').call(d3.axisBottom(_x));
+    svg.select('.y.axis').call(d3.axisRight(_y));
 
 }
--- main.js
+++ main.js
@@ -11,7 +11,13 @@ function bindToTable(element, data){
     
     options['data'] = data;
 
-    new Handsontable(element, options);
+    var table = new Handsontable(element, options);
+
+    table.updateSettings({
+        readOnly: true
+    });
+
+    return table;
 }
 
 function transpose(a) {
@@ -45,6 +51,168 @@ function isCategorical(data){
     return categorical;
 }
 
-function toFinalData(preData, categorical){
+function toFinalData(preData, categorical, inputs, outputs){
+
+    var isInput = JSON.parse(JSON.stringify(categorical));
+
+    //preData and categorical should have the same amount of elements
+    if(preData[0].length != categorical.length){
+        return -1;
+    }
+
+    preData = transpose(preData);
+
+    //get rid of unused columns
+    for(var i = preData.length - 1; i >= 0; i--){
+        if(inputs.indexOf(i) == -1 && outputs.indexOf(i) == -1){
+            preData.splice(i, 1);
+            categorical.splice(i, 1);
+            isInput.splice(i, 1);
+        } else isInput[i] = inputs.indexOf(i) != -1;
+    }
     
+    var classObject = {};
+
+    $.each(categorical, function(index, value){
+
+        if(value){
+
+            //make a list of classes
+            var classes = [];
+            $.each(preData[index], function(innerIndex, innerValue){
+                if($.inArray(innerValue, classes) == -1){
+                    classes.push(innerValue);
+                }
+            });
+
+            var replaceWith = [];
+            $.each(classes, function(innerIndex, innerValue){
+                var temp = [];
+
+                $.each(preData[index], function(innerInnerIndex, innerInnerValue){
+                    if(innerInnerValue == innerValue){
+                        temp.push(1);
+                    } else {
+                        temp.push(0);
+                    }
+                });
+
+                replaceWith.push(temp);
+            });
+
+            var toRepeat = isInput[index];
+
+            preData.splice(index, 1);
+            isInput.splice(index, 1);
+
+
+            $.each(replaceWith, function(innerIndex, innerValue){
+                preData.splice(index, 0, innerValue);
+                isInput.splice(index, 0, toRepeat)
+            });
+            
+            classObject[index] = classes;
+        }
+
+    });
+    
+    return {classes: classObject, data:transpose(preData), isInput:isInput};
+
+}
+
+function createNetwork(layerArray){
+
+    var hiddenLayers = [];
+
+    $.each(layerArray, function(index, value){
+
+        if(index != layerArray.length - 1){
+            layerArray[index].project(layerArray[index + 1]);
+
+            if(index != 0){
+                hiddenLayers.push(value);
+            }
+        }
+    });
+
+    return new synaptic.Network({
+        input: layerArray[0],
+        hidden: hiddenLayers,
+        output: layerArray[layerArray.length - 1]
+    });
+}
+
+function createTrainingData(data, isInput){
+
+    if(data[0].length != isInput.length){
+        console.error('createTrainingData length mismatch');
+        return 'ERROR: length mismatch'
+    }
+
+    data = normalizeData(data);
+
+    var trainingData = [];
+
+    $.each(data, function(index, value){
+
+        var tempInput = [];
+        var tempOutput = [];
+
+        $.each(value, function(innerIndex, innerValue){
+
+            if(isInput[innerIndex]){
+                tempInput.push(innerValue);
+            } else {
+                tempOutput.push(innerValue);
+            }
+
+        });
+
+        trainingData.push({
+            input: tempInput,
+            output: tempOutput
+        });
+    });
+
+    return trainingData;
+}
+
+function normalizeData(data){
+
+    var normalized = transpose(data);
+
+    $.each(normalized, function(index, value){
+
+        var mean = 0;
+        var max = Number.MIN_VALUE;
+        var min = Number.MAX_VALUE;
+        $.each(value, function(innerIndex, innerValue){
+            mean += innerValue;
+            if(innerValue > max){
+                max = innerValue;
+            }
+            if(innerValue < min){
+                min = innerValue;
+            }
+
+        });
+        mean /= value.length;
+
+        var range = max - min;
+
+        $.each(value, function(innerIndex, innerValue){
+            value[innerIndex] -= mean;
+            value[innerIndex] /= range;
+        });
+
+    });
+
+    return transpose(normalized);
+}
+
+function round(number, precision) {
+    var factor = Math.pow(10, precision);
+    var tempNumber = number * factor;
+    var roundedTempNumber = Math.round(tempNumber);
+    return roundedTempNumber / factor;
 }
